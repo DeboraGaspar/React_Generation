1cc6dbb97d025ab23c0305e926fb3db43598b967
Débora Santos Gaspar <deborasgaspar21@gmail.com>
2022-08-05 12:47:17 -0300

    Criando models

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Footer from './componentes/estaticos/footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../src/paginas/home/Home';
 import Login from './paginas/login/Login';
+import ListaTema from './componentes/temas/listaTema/ListaTema';
+import ListaPostagem from './componentes/postagens/listaPostagem/ListaPostagem';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 
@@ -11,16 +13,14 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ minHeight: '100vh' }}>
+      <div style={{ minHeight: "100vh" }}>
         <Routes>
-
           <Route path="/" element={<Login />} />
-
-          <Route path="/home" element={<Home />} />
-
           <Route path="/login" element={<Login />} />
-
+          <Route path="/home" element={<Home />} />
           <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/postagens" element={<ListaPostagem/>}/>
         </Routes>
       </div>
       <Footer />
