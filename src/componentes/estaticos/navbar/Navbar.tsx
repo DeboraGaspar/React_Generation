bc6e60d45fd60ba8c616c87cb1ee6d1e067a490d
Débora Santos Gaspar <deborasgaspar21@gmail.com>
2022-08-05 09:51:32 -0300

    atualizações

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -1,52 +1,66 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-import{ Link } from 'react-router-dom';
-import React from "react";
-import "./Navbar.css";
+import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
-function Navbar() {
+import './NavBar.css'
+
+function NavBar() {
     return (
         <>
             <AppBar position="static">
-                <Toolbar variant="dense" style={{ backgroundColor: "#3F51B5" }}>
-                    <Box className= 'cursor'>
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} className= 'cursor'>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className= 'cursor'>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className= 'cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className= 'cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        <Link to='/login' className="text-decorator-none">
-                            <Box mx={1} className= 'cursor'>
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
-                                    logout
+                                    Home
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/posts" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Postagens
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/temas" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/formularioTema" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Cadastrar Temas
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to="/login" className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
                                 </Typography>
                             </Box>
                         </Link>
                     </Box>
+
                 </Toolbar>
             </AppBar>
         </>
-    );
+    )
 }
 
-export default Navbar;
+export default NavBar
